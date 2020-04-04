3694dc86c36ce18aeee80bfc2fd280181b330154
Jiri Novotny <jnovotny@student.hive.fi>
2020-04-04 14:14:53 +0300

    clean

--- maze/sources/gtools.py
+++
@@ -1,8 +0,0 @@
-def grid_index_to_coords(grid_width, grid_height, index):
-	x = index % grid_width
-	y = index // grid_height
-	return [x, y]
-
-def grid_coords_to_index(grid_height, coords):
-	i = grid_height * coords[1] + coords[0]
-	return i
